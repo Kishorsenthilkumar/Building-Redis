b0bcd5e5f9ed7bf14fac146354f7b88fedadff77
kishor-microsaas <kishorpersonaluse@gmail.com>
2026-04-19 21:18:53 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -90,7 +90,10 @@ async def process_command(parts,writer,database,role):
 
         empty_rdb_hex = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
         rdb_file=bytes.fromhex(empty_rdb_hex)
-        writer.write(f"${str(len(rdb_file))}\r\n{rdb_file}".encode())
+        header=f"${str(len(rdb_file))}".encode()
+        response=header+rdb_file
+        writer.write(response)
+       
 
       await writer.drain()
 
